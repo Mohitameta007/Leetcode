447683c79f7f066a538fd13b0c0e9b363feb581d
Mohitameta007 <ametamohit123@gmail.com>
2026-08-11 17:38:54 +0530

    Time: 0 ms (100%), Space: 9 MB (34.7%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        int i = 0;
+        while(i < s.size())
+        {
+            if(s[i] >= '0' && s[i] <= '9')
+            {
+                if(!st.empty()) st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+            i++;
+        }
+        
+        string ans;
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin() , ans.end());
+
+        return ans;
+        
+    }
+};
