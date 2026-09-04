12f6c9bdfa4bc23a8bc4a2addd492a9270a71210
Mohitameta007 <ametamohit123@gmail.com>
2026-09-04 20:45:07 +0530

    Time: 4 ms (13.74%), Space: 31.5 MB (36.87%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
+        vector<int> maxarr(nums.size() , 0);
+        vector<int> minarr(nums.size() , 0);
+        int maxi = 0;
+        int mini = INT_MAX;
+
         for(int i = 0 ; i < nums.size() ; i++)
         {
-            int maxi = 0;
-            int mini = INT_MAX;
-            for(int j = 0 ; j <= i ; j++)
-            {
-                maxi = max(maxi , nums[j]);
-            }
-            for(int k = i ; k < nums.size() ; k++)
-            {
-                mini = min(mini , nums[k]);
-            }
-            int score = maxi-mini;
+            maxi = max(maxi , nums[i]);
+            maxarr[i] = maxi;
+        }
+        for(int i = nums.size()-1 ; i >= 0 ; i--)
+        {
+            mini = min(mini , nums[i]);
+            minarr[i] = mini;
+        }
+
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            int score = maxarr[i] - minarr[i];
             if(score <= k) return i;
         }
 
