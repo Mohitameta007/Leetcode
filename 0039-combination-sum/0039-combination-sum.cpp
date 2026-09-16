f9f5957823973a1e0aaa21ec99bf05488f39c0aa
Mohitameta007 <ametamohit123@gmail.com>
2026-09-16 14:31:19 +0530

    Time: 3 ms (51.19%), Space: 14 MB (76.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,22 +3,21 @@ public:
 
     void combination(vector<int>& nums , int indx , int target , vector<int>& temp , vector<vector<int>>& ans)
     {
-        if(indx == nums.size())
+        if(target == 0)
         {
             if(target == 0) ans.push_back(temp);
             return;
         }
+        if(indx == nums.size()) return;
 
         if(target >= nums[indx])
         {
             temp.push_back(nums[indx]);
             combination(nums , indx , target-nums[indx] , temp , ans);
             temp.pop_back();
-            combination(nums , indx+1 , target , temp , ans);
         }
-        else {
-            combination(nums , indx+1 , target , temp , ans);
-        }
+        combination(nums , indx+1 , target , temp , ans);
+
 
     }
 
